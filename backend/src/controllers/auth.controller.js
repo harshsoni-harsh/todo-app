758434824b543307aa8471f2259decf91a67e49c
Harsh Soni <52278293+harshsoni-harsh@users.noreply.github.com>
2025-04-30 18:32:43 +0530

    chore: added dockerfiles and nginx config

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -20,7 +20,7 @@ exports.signup = async (req, res) => {
       res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "lax",
         maxAge: 60 * 60 * 1000,
         path: "/",
       });
@@ -60,7 +60,7 @@ exports.login = async (req, res) => {
       res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "lax",
         maxAge: 60 * 60 * 1000,
         path: "/",
       });
